2cf4c7fea9105ce2e30d2cf111233b31cab32e7c
SylwesterKorycki <korycki.sylwester@outlook.com>
2025-09-19 13:21:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
 
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
